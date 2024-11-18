17a6f027d01c8b72eb954073e2417b8530e24e53
Mathis Burger <handymathis@gmail.com>
2024-11-18 16:26:00 +0100

    feat: Added state and fixed some material type implementations

--- src/main/java/de/mathisburger/MaterialTyp.java
+++ src/main/java/de/mathisburger/MaterialTyp.java
@@ -5,22 +5,36 @@ import java.util.Collection;
 
 public class MaterialTyp {
 
-    private long materialNr;
+    private final long materialNr;
 
     private String beschreibung;
 
-    private boolean minimalLagernd;
+    private long minimalLagernd;
+
+    private long kritischerBestand;
 
     private long anzahl;
 
+    private State state;
+
+    /**
+     * This is not used in the current state maschine implementation.
+     */
     private Collection<MaterialExemplar> exemplare;
 
-    public MaterialTyp() {
-        this.materialNr = 0;
-        this.beschreibung = "";
-        this.minimalLagernd = true;
-        this.anzahl = 0;
+    public MaterialTyp(long materialNr, String beschreibung, long minimalLagernd, long anzahl, long kritischerBestand) {
+
+        if (kritischerBestand >= minimalLagernd) {
+            throw new RuntimeException("Der kritische Bestand muss kleiner sein, als der minimal lagernde bestand");
+        }
+
+        this.materialNr = materialNr;
+        this.beschreibung = beschreibung;
+        this.minimalLagernd = minimalLagernd;
+        this.kritischerBestand = kritischerBestand;
+        this.anzahl = anzahl;
         this.exemplare = new ArrayList<MaterialExemplar>();
+        this.state = State.Locked;
     }
 
     public long getAnzahl() {
@@ -55,15 +69,70 @@ public class MaterialTyp {
         return materialNr;
     }
 
-    public void setMaterialNr(long materialNr) {
-        this.materialNr = materialNr;
-    }
-
-    public boolean isMinimalLagernd() {
+    public long isMinimalLagernd() {
         return minimalLagernd;
     }
 
-    public void setMinimalLagernd(boolean minimalLagernd) {
+    public void setMinimalLagernd(long minimalLagernd) {
         this.minimalLagernd = minimalLagernd;
+    }
+
+    public long getKritischerBestand() {
+        return kritischerBestand;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    private void setState(State state) {
+        this.state = state;
+    }
+
+    public void setKritischerBestand(long kritischerBestand) {
+        this.kritischerBestand = kritischerBestand;
+    }
+
+    // --------------- Functionality implementations here
+
+    public void lieferungEinlagern(long anzahl) {
+        if (anzahl <= 0) {
+            throw new RuntimeException("Negative numbers not allowed");
+        }
+        this.anzahl += anzahl;
+        if (this.anzahl > this.kritischerBestand) {
+            this.setState(State.Locked);
+        }
+        if (this.anzahl > this.minimalLagernd) {
+            this.setState(State.InStock);
+        }
+    }
+
+    public void entnahmeDurchfuehren(long anzahl) {
+        if (anzahl <= 0) {
+            throw new RuntimeException("Negative numbers not allowed");
+        }
+        if (this.state != State.InStock) {
+            throw new RuntimeException("Es kann nichts aus dem Bestand entnommen werden");
+        }
+        this.anzahl -= anzahl;
+        if (this.anzahl <= this.minimalLagernd && this.anzahl > this.kritischerBestand) {
+            this.state = State.MinimalStock;
+            this.placeRestockOrder();
+        }
+        if (this.anzahl < this.kritischerBestand) {
+            this.sendCriticalMail();
+            this.placeRestockOrder();
+            this.state = State.Critial;
+        }
+
+    }
+
+    private void sendCriticalMail() {
+        System.out.println("Wir befinden uns im kritischen Bestand!!!!");
+    }
+
+    private void placeRestockOrder() {
+        System.out.println("Es wurde Material nachbestellt");
     }
 }
---
+++ src/main/java/de/mathisburger/State.java
@@ -0,0 +1,8 @@
+package de.mathisburger;
+
+public enum State {
+    Locked,
+    InStock,
+    Critial,
+    MinimalStock
+}
